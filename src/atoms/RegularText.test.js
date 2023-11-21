1c41468f0d0af797fde696e1a820469ff27d261c
rune.leemans <rune.leemans@student.odisee.be>
2023-11-21 09:27:47 +0100

    Test van text components en text components

---
+++ src/atoms/RegularText.test.js
@@ -0,0 +1,13 @@
+// src/atoms/Button.test.js
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import RegularText from "./RegularText";
+
+describe("RegularText component", () => {
+  it("Renders regular text correctly", () => {
+    render(<RegularText>Text</RegularText>);
+    const title = screen.getByText("Text");
+    expect(title).toBeInTheDocument();
+  });
+});
