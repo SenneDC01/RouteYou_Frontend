acb837efe13b05603ac8f2337f0bdf22d6a14bea
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-11-22 14:34:13 +0100

    privacyverklaring

--- src/app/events/[id]/page.js
+++ src/app/events/[id]/page.js
@@ -7,29 +7,29 @@ import Header from "@/organisms/header";
 import Footer from "@/organisms/footer";
 import DetailColumn from "@/organisms/DetailColumn";
 
-export default function Page({ params }) {
-  const event = {
-    id: 1,
-    name: "City Light Run Aalst",
-    description: "Loop langs de mooiste parels van Aalst",
-    start_date: "2024-04-22 21:00:00",
-    max_participants: 1000,
-    price: "5.00",
-    visibility: "PUBLIC",
-    image_url: "http://localhost:8080/storage/images/no-profile-picture.png",
-    author: "Senna Uyttersprot",
-    routes: [
-      {
-        route_data: {
-          id: 6833170,
-          duration: "1.2km",
-          startAddress: "Aalst, Oost-Vlaanderen, Vlaanderen",
-          type: "Looproute",
-          difficulty: 0.3,
-        },
-      },
-    ],
-  };
+export default function Page({ event }) {
+  // const event = {
+  //   id: 1,
+  //   name: "City Light Run Aalst",
+  //   description: "Loop langs de mooiste parels van Aalst",
+  //   start_date: "2024-04-22 21:00:00",
+  //   max_participants: 1000,
+  //   price: "5.00",
+  //   visibility: "PUBLIC",
+  //   image_url: "http://localhost:8080/storage/images/no-profile-picture.png",
+  //   author: "Senna Uyttersprot",
+  //   routes: [
+  //     {
+  //       route_data: {
+  //         id: 6833170,
+  //         duration: "1.2km",
+  //         startAddress: "Aalst, Oost-Vlaanderen, Vlaanderen",
+  //         type: "Looproute",
+  //         difficulty: 0.3,
+  //       },
+  //     },
+  //   ],
+  // };
   const [key, setKey] = useState(true);
   const [value, setValue] = useState("");
 
