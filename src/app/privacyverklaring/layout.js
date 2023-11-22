f015580e77f5350d92983afe07233d6514168fc8
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-11-22 14:58:22 +0100

    evaluatierapport

---
+++ src/app/privacyverklaring/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Privacyverklaring - RouteYou",
+  description:
+    "Privacyverklaring van RouteYou.",
+  keywords: "RouteYou, privacy, verklaring, privacyverklaring",
+};
+export default function PrivacyverklaringLayout({ children }) {
+    return children
+}
