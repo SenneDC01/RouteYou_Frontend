c736326efe062e4e87b26bcdfd7bc23cc1367f52
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-03 18:44:48 +0100

    Update error message to smallText component

--- src/components/atoms/form-field/FormField.jsx
+++ src/components/atoms/form-field/FormField.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./FormField.module.scss";
-import SubText from "../regular-text/RegularText";
+import SmallText from "../small-text/SmallText";
 
 const FormField = ({ label, placeholder, value, name, type, onChange, errorMessage }) => {
   return (
@@ -15,7 +15,7 @@ const FormField = ({ label, placeholder, value, name, type, onChange, errorMessa
         placeholder={placeholder}
         onChange={onChange}
       />
-      {errorMessage && <SubText className={styles.error}>{ errorMessage }</SubText>}
+      {errorMessage && <SmallText className={styles.error}>{ errorMessage }</SmallText>}
     </div>
   );
 };
