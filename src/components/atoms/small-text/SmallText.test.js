c147deab04256807e72ae179d215ddefb6792d76
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-03 18:43:56 +0100

    Test: SmallText

---
+++ src/components/atoms/small-text/SmallText.test.js
@@ -0,0 +1,13 @@
+// src/atoms/ButtonRound.test.js
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import SmallText from "./SmallText";
+
+describe("SmallText component", () => {
+  it("Renders small text correctly", () => {
+    render(<SmallText>Text</SmallText>);
+    const title = screen.getByText("Text");
+    expect(title).toBeInTheDocument();
+  });
+});
