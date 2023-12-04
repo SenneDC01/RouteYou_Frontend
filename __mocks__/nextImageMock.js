a11ddc3d30869121f0f644d61ce6c318deffdc26
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-04 20:27:15 +0100

    jest config update for image imports

---
+++ __mocks__/nextImageMock.js
@@ -0,0 +1,9 @@
+// __mocks__/nextImageMock.js
+
+const NextImage = ({ src, alt, priority, ...rest }) => {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={src} alt={alt} {...rest} data-priority={priority} />;
+  };
+  
+  export default NextImage;
+  
