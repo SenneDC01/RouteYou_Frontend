8cf4478c70104c0a1d67350bd25717e7518320c3
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-05 14:16:02 +0100

    Test: Update new testcase for login call to API

--- src/app/login/LoginPage.test.js
+++ src/app/login/LoginPage.test.js
@@ -1,17 +1,40 @@
 import { render, fireEvent } from "@testing-library/react";
 import Page from "./page";
+import * as UserService from "@/services/UserService";
+import { act } from "react-test-renderer";
 
 describe("LoginPage component", () => {
-  it("Log in with valid credantials", () => {
+  let mockLogin;
+
+  beforeEach(() => {
+    mockLogin = jest.spyOn(UserService, 'login');
+    mockLogin.mockImplementation(() => Promise.resolve());
+  });
+
+  afterEach(() => {
+    mockLogin.mockRestore();
+  });
+
+  it("Log in with valid credentials", async () => {
     const { getByLabelText, getByRole } = render(<Page />);
+    const email = "test@example.com";
+    const password = "password123";
 
-    fireEvent.change(getByLabelText("Email"), {
-      target: { value: "test@example.com" },
-    });
-    fireEvent.change(getByLabelText("Password"), {
-      target: { value: "password123" },
+    await act(async () => {
+      fireEvent.change(getByLabelText("Email"), {
+        target: { value: email },
+      });
+      fireEvent.change(getByLabelText("Password"), {
+        target: { value: password },
+      });
+
+      fireEvent.click(getByRole("button", { name: "Login" }));
     });
 
-    fireEvent.click(getByRole("button", { name: "Login" }));
+    expect(mockLogin).toHaveBeenCalled();
+    expect(mockLogin).toHaveBeenCalledWith({
+      email: email,
+      password: password,
+    });
   });
 });
