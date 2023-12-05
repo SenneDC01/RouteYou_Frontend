66589caf3f95403afd7f814203d46888d3a370ee
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-05 16:29:08 +0100

    Test: Register

---
+++ src/app/register/Register.test.js
@@ -0,0 +1,75 @@
+import { render, fireEvent } from "@testing-library/react";
+import RegisterPage from "./page";
+import * as UserService from "@/services/UserService";
+import { act } from "react-test-renderer";
+
+describe("RegisterPage component", () => {
+  let mockRegister;
+
+  beforeEach(() => {
+    mockRegister = jest.spyOn(UserService, "register");
+    mockRegister.mockImplementation(() => Promise.resolve());
+  });
+
+  afterEach(() => {
+    mockRegister.mockRestore();
+  });
+
+  it("Register with valid credentials", async () => {
+    const { getByLabelText, getByRole } = render(<RegisterPage />);
+    const firstname = "Pieter";
+    const lastname = "Post";
+    const gender = "MALE";
+    const email = "test@example.com";
+    const address = "Straat 1";
+    const city = "Gent";
+    const zipcode = "9300";
+    const password = "password123";
+    const password_confirmation = "password123";
+
+    await act(async () => {
+      fireEvent.change(getByLabelText("Firstname"), {
+        target: { value: firstname },
+      });
+      fireEvent.change(getByLabelText("Lastname"), {
+        target: { value: lastname },
+      });
+      fireEvent.change(getByLabelText("Gender"), {
+        target: { value: gender },
+      });
+      fireEvent.change(getByLabelText("Email"), {
+        target: { value: email },
+      });
+      fireEvent.change(getByLabelText("Address"), {
+        target: { value: address },
+      });
+      fireEvent.change(getByLabelText("City"), {
+        target: { value: city },
+      });
+      fireEvent.change(getByLabelText("Zipcode"), {
+        target: { value: zipcode },
+      });
+      fireEvent.change(getByLabelText("Password"), {
+        target: { value: password },
+      });
+      fireEvent.change(getByLabelText("Confirm Password"), {
+        target: { value: password_confirmation },
+      });
+
+      fireEvent.click(getByRole("button", { name: "Register" }));
+    });
+
+    expect(mockRegister).toHaveBeenCalled();
+    expect(mockRegister).toHaveBeenCalledWith({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      gender: gender,
+      password: password,
+      password_confirmation: password_confirmation,
+      address: address,
+      city: city,
+      zipcode: zipcode,
+    });
+  });
+});
