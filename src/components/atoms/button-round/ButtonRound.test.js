e2dbd758d1511e301df3b9527b3152b50cc266c4
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-05 11:04:13 +0100

    Update RoundButton

--- src/components/atoms/button-round/ButtonRound.test.js
+++ src/components/atoms/button-round/ButtonRound.test.js
@@ -2,13 +2,19 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Button from "./ButtonRound";
+import ButtonRound from "./ButtonRound";
 import ArrowRightSVG from "@/utils/icons/ArrowRightSVG";
 
 describe("Button component", () => {
   it("renders button with icon", () => {
-    render(<Button icon={<ArrowRightSVG />} />);
-    const buttonElement = screen.getByLabelText("forward button");
+    render(
+      <ButtonRound
+        href="/"
+        ariaLabel="Ga naar event"
+        icon={<ArrowRightSVG />}
+      />
+    );
+    const buttonElement = screen.getByLabelText("Ga naar event");
     expect(buttonElement).toBeInTheDocument();
   });
 });
