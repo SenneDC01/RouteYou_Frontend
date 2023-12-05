59258fef3fff6242af77fbfd4d74fe6caca35b60
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-05 10:12:24 +0100

    Button component edit

--- src/components/atoms/button/Button.js
+++ src/components/atoms/button/Button.js
@@ -3,27 +3,21 @@ import React from "react";
 import styles from "./Button.module.scss";
 
 const Button = ({
-  label = "Click me",
   children,
   onClick,
-  type,
-  link = "#",
+  type = "button",
   icon = null,
   buttonStyles,
 }) => {
   return (
-    // <a
-    //   className={[styles.buttonStyle, buttonStyles].join(" ")}
-    //   aria-label="forward button"
-    //   href={link}
-    // >
-    //   <div className={[styles.buttonSpacing]}>
-    //     {icon && React.cloneElement(icon, { width: "1.5rem", height: "1.5rem" })}
-    //     {label}
-    //   </div>
-    // </a>
-
-    <button type={type} onClick={onClick} className={[styles.buttonStyle, buttonStyles].join(" ")}>{ children }</button>
+    <button
+      type={type}
+      onClick={onClick}
+      className={[styles.buttonStyle, buttonStyles].join(" ")}
+    >
+      {icon && <span className={styles.icon}>{icon}</span>}
+      {children}
+    </button>
   );
 };
 
