35957fc9583ce1f0c7172fecfc4c90f100a44816
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-05 20:04:17 +0100

    Update RoundButton Component

--- src/components/atoms/button-round/ButtonRound.jsx
+++ src/components/atoms/button-round/ButtonRound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./ButtonRound.module.scss";
 import Link from "next/link";
 
-const ButtonRound = ({ href = "#", ariaLabel = "forward-button", icon = null, className }) => {
+const ButtonRound = ({ href, ariaLabel = "forward-button", icon, className }) => {
   return (
     <Link
       href={href}
