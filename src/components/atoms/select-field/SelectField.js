899a06022f5090fb3a4d9fe6a8df95e101c0792a
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-05 20:07:51 +0100

    Selectfield Component

---
+++ src/components/atoms/select-field/SelectField.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "@/components/atoms/form-field/FormField.module.scss";
+import SmallText from "../small-text/SmallText";
+
+const SelectField = ({
+  label,
+  name,
+  options,
+  onChange,
+  value,
+  errorMessage,
+  className,
+}) => {
+  return (
+    <div className={[styles.formField, className].join(" ")}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <select
+        id={name}
+        name={name}
+        value={value || ""}
+        onChange={onChange}
+        className={[styles.field, errorMessage ? styles.inValid : ""].join(" ")}
+      >
+        <option value="" disabled>
+          Selecteer een optie
+        </option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {errorMessage && (
+        <SmallText className={styles.error}>{errorMessage}</SmallText>
+      )}
+    </div>
+  );
+};
+
+export default SelectField;
