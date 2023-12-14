91e902d0b5765a970b7651ba4f279e446a502b3f
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2023-12-14 16:30:07 +0100

    favbutton fix

--- src/components/atoms/fav-button/FavButton.jsx
+++ src/components/atoms/fav-button/FavButton.jsx
@@ -1,16 +1,15 @@
 "use client"
 import React, { useState } from "react";
 import styles from "./FavButton.module.scss";
-// import StarSVG from "@/utils/icons/StarSVG";
-import StarSVG from "../../../utils/icons/StarSVG";
+import StarSVG from "@/utils/icons/StarSVG";
 
 const FavButton = () => {
   const [isFavorite, setIsFavorite] = useState(false);
 
   const handleToggleFavorite = () => {
     setIsFavorite(!isFavorite);
-    console.log("fav");
   };
+  
   return (
     <button
       className={[styles.iconButton]}
