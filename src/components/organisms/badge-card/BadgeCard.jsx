67ef4879763657b612297f27c1516b7cf68b9f04
SenneDeC <102857178+SenneDeC@users.noreply.github.com>
2023-12-17 13:32:39 +0100

    removed comments

--- src/components/organisms/badge-card/BadgeCard.jsx
+++ src/components/organisms/badge-card/BadgeCard.jsx
@@ -1,5 +1,3 @@
-// EventCard.jsx
-
 import React from "react";
 import PropTypes from "prop-types";
 import Image from "next/image";
@@ -9,38 +7,38 @@ import MedalSVG from "@/utils/icons/MedalSVG";
 import styles from "./BadgeCard.module.scss";
 
 const BadgeCard = ({
-                       image,
-                       title = "Groene Gordel 2024",
-                       organisator = "organisator",
-                       dateTime = "date - time",
-                   }) => {
-    return (
-        <div data-testid="badgeCard" className={[styles.badgeCard]}>
-            <Image
-                height={300}
-                width={300}
-                alt={title}
-                className={[styles.badgeImage]}
-                src={image}
-            />
-            <div className={[styles.badgeRowContainer]}>
-                <div className={[styles.badgeDetails]}>
-                    <BoldText>{title}</BoldText>
-                    <SubText>
-                        {organisator} <br /> {dateTime}
-                    </SubText>
-                </div>
-                <MedalSVG width={60} height={60} />
-            </div>
+  image,
+  title = "Groene Gordel 2024",
+  organisator = "organisator",
+  dateTime = "date - time",
+}) => {
+  return (
+    <div data-testid="badgeCard" className={[styles.badgeCard]}>
+      <Image
+        height={300}
+        width={300}
+        alt={title}
+        className={[styles.badgeImage]}
+        src={image}
+      />
+      <div className={[styles.badgeRowContainer]}>
+        <div className={[styles.badgeDetails]}>
+          <BoldText>{title}</BoldText>
+          <SubText>
+            {organisator} <br /> {dateTime}
+          </SubText>
         </div>
-    );
+        <MedalSVG width={60} height={60} />
+      </div>
+    </div>
+  );
 };
 
 BadgeCard.propTypes = {
-    image: PropTypes.object,
-    title: PropTypes.string,
-    organisator: PropTypes.string,
-    dateTime: PropTypes.string,
+  image: PropTypes.object,
+  title: PropTypes.string,
+  organisator: PropTypes.string,
+  dateTime: PropTypes.string,
 };
 
 export default BadgeCard;
