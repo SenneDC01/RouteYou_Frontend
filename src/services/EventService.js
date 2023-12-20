d07dff4b6eb14b7d3bdba0567b3a18c27414451f
senne.decock <senne.decock@student.odisee.be>
2023-12-20 12:27:35 +0100

    GetParticipants service

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -56,3 +56,27 @@ export const interestedEvents = async () => {
 
 // Get participants
 //http://10.129.23.226:8080/api/documentation#/Events/getParticipants
+export const getParticipants = async () => {
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), 10000);
+
+  try {
+    const response = await fetch(`${API_URL}/events/getParticipants`, {
+      method: 'GET',
+      headers: HEADERS,
+      signal: controller.signal,
+    });
+
+    if (!response.ok) {
+      return null;
+    }
+
+    const data = await response.json();
+
+    return data.events.data;
+  } catch (error) {
+    return null;
+  } finally {
+    clearTimeout(timeoutId);
+  }
+};
