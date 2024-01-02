b764cf76f480033d1d058b411242e76ce2aa16b3
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-02 13:34:54 +0100

    Test: Secondary Button

---
+++ src/components/atoms/secondary-button/SecondaryButton.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SecondaryButton from './SecondaryButton';
+import AddSVG from '@/utils/icons/AddSVG';
+
+describe('Secondary Button component', () => {
+  it('renders button with label and icon', () => {
+    render(<SecondaryButton icon={<AddSVG/>}>Click me</SecondaryButton>);
+    const buttonElement = screen.getByText('Click me');
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('renders button with label and type', () => {
+    render(<SecondaryButton type='submit'>Click me</SecondaryButton>);
+    const buttonElement = screen.getByText('Click me');
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
