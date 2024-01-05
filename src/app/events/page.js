3a875172ad64dcb0a963bdd4757f9abb95e8e379
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-05 11:42:18 +0100

    EventsPage to show all public events without searchbar

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,5 +1,35 @@
-import React from 'react';
+'use client';
+import BigTitle from '@/components/atoms/big-title/BigTitle';
+import EventCard from '@/components/organisms/event-card/EventCard';
+import { publicEvents } from '@/services/EventService';
+import React, { useEffect, useState } from 'react';
+import styles from './EventsPage.module.scss';
 
-export default function Page({ params }) {
-  return <div>Event: {params.id}</div>;
+export default function EventsPage() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await publicEvents();
+      setEvents(response.events.data);
+    };
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.searchBar}>
+        <p>SearchBar</p>
+      </div>
+      <div className={styles.events}>
+        <BigTitle>Events</BigTitle>
+        <div className={styles.eventsContainer}>
+          {events.map((event, index) => (
+            <EventCard event={event} key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
