beac89cb3a6dda5345fad9fe1519745e9a4f78e5
Senne De Cock <senne.decock@student.odisee.be>
2024-01-09 15:26:38 +0000

    Remove comments

--- src/components/molecules/status-drop-down/StatusDropdown.test.js
+++ src/components/molecules/status-drop-down/StatusDropdown.test.js
@@ -6,21 +6,15 @@ import '@testing-library/jest-dom';
 test('Participants renders correctly and triggers onStatusChange', () => {
   render(<StatusDropdown />);
 
-  // Check if the default selected option is rendered
   expect(screen.getByText('Interested')).toBeInTheDocument();
 
-  // Open the dropdown
   fireEvent.click(screen.getByText('Interested'));
 
-  // Click on the 'Signed Up' option
   fireEvent.click(screen.getByText('Signed Up'));
 
-  // Check if the selected option is updated
   expect(screen.getByText('Signed Up')).toBeInTheDocument();
 
-  // Click on the 'Canceled' option
   fireEvent.click(screen.getByText('Canceled'));
 
-  // Check if the selected option is updated
   expect(screen.getByText('Canceled')).toBeInTheDocument();
 });
