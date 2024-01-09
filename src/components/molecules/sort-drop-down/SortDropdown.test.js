48707833d05b9bd8037cdbb6e00ff9b0889b60fb
Senne De Cock <senne.decock@student.odisee.be>
2024-01-09 15:26:06 +0000

    Remove comments

--- src/components/molecules/sort-drop-down/SortDropdown.test.js
+++ src/components/molecules/sort-drop-down/SortDropdown.test.js
@@ -6,15 +6,11 @@ import '@testing-library/jest-dom';
 test('SortDropdown renders correctly and triggers onSortChange', () => {
   render(<SortDropdown />);
 
-  // Check if the default selected option is rendered
   expect(screen.getByText('Name')).toBeInTheDocument();
 
-  // Open the dropdown
   fireEvent.click(screen.getByText('Name'));
 
-  // Click on the 'Date' option
   fireEvent.click(screen.getByText('Date'));
 
-  // Check if the selected option is updated
   expect(screen.getByText('Date')).toBeInTheDocument();
 });
