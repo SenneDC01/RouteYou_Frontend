fc7cf0a51c0699223a437414d0c176bc57f4fdbf
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-13 15:27:15 +0100

    display no events message & fix W3C error of div in ol

--- src/app/dashboard/my-events/page.js
+++ src/app/dashboard/my-events/page.js
@@ -4,21 +4,25 @@ import DashboardContent from '@/components/organisms/dashboard-content/Dashboard
 import EventCard from '@/components/organisms/event-card/EventCard';
 import styles from '../DashboardPage.module.scss';
 import '../DashboardStyling.css';
+import TextLink from '@/components/atoms/text-link/TextLink';
 
 export default async function Page({ events }) {
   return (
     <div className={styles.dashboard}>
       <DashboardNav />
-      {events?.length && (
-        <DashboardContent
-          title="My Events"
-          description="Here you will be able to see the events you organize."
-        >
-          {events?.map((e, index) => {
+
+      <DashboardContent
+        title="My Events"
+        description="Here you will be able to see the events you organize."
+      >
+        {events.length &&
+          events.map((e, index) => {
             return <EventCard key={index} event={e}></EventCard>;
           })}
-        </DashboardContent>
-      )}
+        {!events.length && (
+          <TextLink href="/events/create">Create your first event</TextLink>
+        )}
+      </DashboardContent>
     </div>
   );
 }
