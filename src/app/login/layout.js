887468ac600cf8f4c276594406725856959a4204
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-13 17:45:41 +0100

    metadata for loginpage

---
+++ src/app/login/layout.js
@@ -0,0 +1,11 @@
+export function generateMetadata() {
+  return {
+    title: 'Login - RouteYou',
+    description: 'Login with your RouteYou account.',
+    keywords: 'login, RouteYou, account',
+  };
+}
+
+export default async function EventsPage({ children }) {
+  return children;
+}
