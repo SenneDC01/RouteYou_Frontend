95115c42aa93307c3e051527f3735ecab59b3bbb
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-13 17:43:28 +0100

    delete dashboard page

--- src/app/dashboard/page.js
+++
@@ -1,7 +0,0 @@
-'use client';
-import React from 'react';
-import DashboardNav from '@/components/organisms/dashboard-nav/DashboardNav';
-
-export default function Page() {
-  return <DashboardNav />;
-}
