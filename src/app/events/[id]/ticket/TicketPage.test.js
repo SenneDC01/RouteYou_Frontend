cc918983db45864de7054ff89b8c799d248b84af
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-18 19:10:51 +0100

    Test: Updat test with new response from API

--- src/app/events/[id]/ticket/TicketPage.test.js
+++ src/app/events/[id]/ticket/TicketPage.test.js
@@ -6,54 +6,36 @@ import '@testing-library/jest-dom';
 const ticketResponseData = {
   message: 'Here is your ticket',
   data: {
-    name: 'City Light Run Aalst',
-    author: 'Senna Uyttersprot',
-    start_date: '2024-04-22 21:00:00',
-    qr_code: 'QR-CODE_65a01b4f2713f.svg',
-  },
-  code: 200,
-};
-
-const eventResponseData = {
-  message: 'Event has been fetched successfully',
-  event: {
     id: 1,
     name: 'City Light Run Aalst',
     description: 'Loop langs de mooiste parels van Aalst',
     start_date: '2024-04-22 21:00:00',
-    max_participants: 1000,
-    price: '5.00',
-    visibility: 'PUBLIC',
     image_url:
       'https://storage.googleapis.com/picture-staging-laravel-bucket/images/events/1/citylightrunaalst.jpg',
     author: 'Senna Uyttersprot',
     routes: [[Object]],
+    qr_code:
+      'https://storage.googleapis.com/picture-staging-laravel-bucket/images/events/1/QR-CODE_65a968644b6f7.svg',
   },
   code: 200,
 };
 
 describe('ticketpage', () => {
   let mockTicket;
-  let mockEvent;
 
   beforeEach(() => {
     mockTicket = jest.spyOn(EventService, 'eventTicket');
     mockTicket.mockImplementation(() => Promise.resolve(ticketResponseData));
-
-    mockEvent = jest.spyOn(EventService, 'eventDetail');
-    mockEvent.mockImplementation(() => Promise.resolve(eventResponseData));
   });
 
   afterEach(() => {
     mockTicket.mockRestore();
-    mockEvent.mockRestore();
   });
 
   it('displays the ticket of the event', async () => {
     render(await TicketPage({ params: { id: 1 } }));
 
     await waitFor(() => {
-      expect(mockEvent).toHaveBeenCalled();
       expect(mockTicket).toHaveBeenCalled();
     });
 
@@ -70,7 +52,9 @@ describe('ticketpage', () => {
     );
     expect(img).toHaveAttribute(
       'src',
-      expect.stringContaining('QR-CODE_65a01b4f2713f.svg')
+      expect.stringContaining(
+        'https://storage.googleapis.com/picture-staging-laravel-bucket/images/events/1/QR-CODE_65a968644b6f7.svg'
+      )
     );
   });
 
