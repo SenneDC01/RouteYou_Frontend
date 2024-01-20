47a5a2928b69d418b94c528b035b160cfe023904
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-20 12:21:01 +0100

    Test: Update favbutton for call to eventService

--- src/components/atoms/fav-button/FavButton.test.js
+++ src/components/atoms/fav-button/FavButton.test.js
@@ -1,12 +1,28 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import FavButton from '@/components/atoms/fav-button/FavButton';
+import * as EventService from '@/services/EventService';
 
 describe('FavButton', () => {
-  it('toggles favorite status on click', () => {
-    const { getByRole } = render(<FavButton />);
+  let mockEventsService;
+
+  beforeEach(() => {
+    mockEventsService = jest.spyOn(EventService, 'markAsInterested');
+    mockEventsService.mockImplementation(() => Promise.resolve({ code: 200 }));
+  });
+
+  afterEach(() => {
+    mockEventsService.mockRestore();
+  });
+
+  it('toggles favorite status on click',async () => {
+    const { getByRole } = render(
+      <FavButton event={{ id: 1, relation: null }} />
+    );
 
     fireEvent.click(getByRole('button'));
+    await waitFor(() => expect(mockEventsService).toHaveBeenCalled());
     expect(getByRole('button')).toBeTruthy();
+    expect(getByRole('button').firstChild).toHaveAttribute('fill', '#1a614a');
   });
 });
