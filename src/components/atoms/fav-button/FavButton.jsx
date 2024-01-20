e306e8b976d42f3c5380982e685ab56c4cb83a90
Senna Uyttersprot <senna.uyttersprot@student.odisee.be>
2024-01-20 12:21:19 +0100

    Call eventService when button is clicked

--- src/components/atoms/fav-button/FavButton.jsx
+++ src/components/atoms/fav-button/FavButton.jsx
@@ -1,8 +1,19 @@
+'use client';
+import { markAsInterested } from '@/services/EventService';
 import StarSVG from '@/utils/icons/StarSVG';
+import { useState } from 'react';
 
-export default function FavButton({ onClick, isFavorite }) {
+export default function FavButton({ event }) {
+  const [isFavorite, setIsFavorite] = useState(event.relation === 'INTERESTED');
+
+  const makeInterested = async () => {
+    const response = await markAsInterested(event.id);
+    if (response.code === 200) {
+      setIsFavorite(true);
+    }
+  };
   return (
-    <button aria-label="mark as interested" onClick={onClick}>
+    <button aria-label="mark as interested" onClick={makeInterested}>
       <StarSVG width={30} height={30} fill={isFavorite ? '#1a614a' : 'none'} />
     </button>
   );
