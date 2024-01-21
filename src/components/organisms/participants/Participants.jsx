6cd6f568de79acaa7dd72688178e31926ad71dee
Senne De Cock <senne.decock@student.odisee.be>
2024-01-21 22:18:41 +0100

    Participants

--- src/components/organisms/participants/Participants.jsx
+++ src/components/organisms/participants/Participants.jsx
@@ -1,44 +1,52 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {getParticipants} from '@/services/EventService';
 import StatusDropdown from '@/components/molecules/status-drop-down/StatusDropdown';
 import SortDropdown from '@/components/molecules/sort-drop-down/SortDropdown';
 import styles from './Participants.module.scss';
 import InviteButton from '@/components/atoms/button/Button';
 import InviteUsersFrame from '@/components/molecules/invite-users-frame/InviteUsersFrame';
-// import { redirect } from 'next/navigation';
 
-const getEventParticipants = async (eventId) => {
-  try {
-    const response = await getParticipants(eventId);
+export default function Participants({event}) {
+  const [participants, setParticipants] = useState(null);
+  console.log(participants);
 
-    if (response.code !== 200) {
-      // redirect('/');
-    }
-    return response;
-  } catch (error) {
-    // redirect('/');
-  }
-};
-export default async function Participants({event}) {
-  const {participants} = await getEventParticipants(event.id);
-  console.log(participants)
+  useEffect(() => {
+    const fetchParticipants = async () => {
+      try {
+        const response = await getParticipants(event.id);
+        console.log(response);
+
+        if (response && response.code === 200) {
+          setParticipants(response.participants.data);
+        } else {
+          console.error('Non-200 status code received or response is null');
+        }
+      } catch (error) {
+        console.error('Error fetching participants:', error);
+      }
+    };
+
+    fetchParticipants();
+  }, [event.id]);
+
+
 
   const [searchQuery, setSearchQuery] = useState('');
   const [sortKey, setSortKey] = useState(null);
   const [isInviteFrameOpen, setIsInviteFrameOpen] = useState(false);
 
-  const filteredParticipants = participants
-      .filter((participant) =>
-          participant.name.toLowerCase().includes(searchQuery.toLowerCase())
-      )
-      .sort((a, b) => {
-        if (sortKey === 'name') {
-          return a.name.localeCompare(b.name);
-        } else if (sortKey === 'date') {
-          return new Date(a.date) - new Date(b.date);
-        }
-        return 0;
-      });
+  // const filteredParticipants = participants
+  //     .filter((participant) =>
+  //         participant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  //     )
+  //     .sort((a, b) => {
+  //       if (sortKey === 'name') {
+  //         return a.name.localeCompare(b.name);
+  //       } else if (sortKey === 'date') {
+  //         return new Date(a.date) - new Date(b.date);
+  //       }
+  //       return 0;
+  //     });
 
   const handleSortChange = (key) => {
     setSortKey(key);
@@ -84,12 +92,12 @@ export default async function Participants({event}) {
           </div>
         </div>
         <ul>
-          {filteredParticipants.map((participant) => (
-              <li key={participant.id} className={styles.list}>
-                {participant.name}
-                <StatusDropdown/>
-              </li>
-          ))}
+          {/*{filteredParticipants.map((participant) => (*/}
+          {/*    <li key={participant.id} className={styles.list}>*/}
+          {/*      {participant.name}*/}
+          {/*      <StatusDropdown/>*/}
+          {/*    </li>*/}
+          {/*))}*/}
         </ul>
         {isInviteFrameOpen && (
             <InviteUsersFrame
