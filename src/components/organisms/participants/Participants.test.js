3036916322c9d3cb30e6205594a2bc236ff8bd51
Senne De Cock <senne.decock@student.odisee.be>
2024-01-22 15:17:04 +0100

    participants test fix

--- src/components/organisms/participants/Participants.test.js
+++ src/components/organisms/participants/Participants.test.js
@@ -7,11 +7,14 @@ import Participants from './Participants';
 jest.mock('@/services/EventService');
 
 const mockParticipants = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Jane Doe' },
+  { id: 1, full_name: 'John Doe', status: 'SIGNED_UP' },
+  { id: 2, full_name: 'Jane Doe', status: 'CANCELED' },
 ];
 
-getParticipants.mockResolvedValue(mockParticipants);
+getParticipants.mockResolvedValue({
+  code: 200,
+  participants: { data: mockParticipants },
+});
 
 describe('Participants Component', () => {
   afterEach(() => {
@@ -19,7 +22,7 @@ describe('Participants Component', () => {
   });
 
   it('renders participants from API or mock data', async () => {
-    const { getByText } = render(<Participants />);
+    const { getByText } = render(<Participants event={{ id: 1 }} />);
 
     await waitFor(() => {
       expect(getByText('John Doe')).toBeInTheDocument();
@@ -29,7 +32,7 @@ describe('Participants Component', () => {
 
   it('filters participants based on search query', async () => {
     const { getByPlaceholderText, getByText, queryByText } = render(
-      <Participants />
+      <Participants event={{ id: 1 }} />
     );
 
     await waitFor(() => {
@@ -46,7 +49,7 @@ describe('Participants Component', () => {
   });
 
   it('sorts participants by name', async () => {
-    const { getByText } = render(<Participants />);
+    const { getByText } = render(<Participants event={{ id: 1 }} />);
 
     await waitFor(() => {
       expect(getByText('John Doe')).toBeInTheDocument();
@@ -54,14 +57,16 @@ describe('Participants Component', () => {
     });
 
     fireEvent.click(getByText('Sort'));
-    fireEvent.click(getByText('Name'));
+    fireEvent.click(getByText('Date'));
 
     expect(getByText('Jane Doe')).toBeInTheDocument();
     expect(getByText('John Doe')).toBeInTheDocument();
   });
 
   it('opens and closes the InviteUsersFrame', async () => {
-    const { getByText, getByRole, queryByRole } = render(<Participants />);
+    const { getByText, getByRole, queryByRole } = render(
+      <Participants event={{ id: 1 }} />
+    );
 
     await waitFor(() => {
       expect(queryByRole('dialog')).toBeNull();
